e7f1bdac5eafa73c4068802bf5f3ca4e5989140c
Anton Khomchenko <anton@khomchenko.com>
2023-05-22 14:30:31 +0100

    fix image route

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image src={"/static/img/icon.png"} className={styles.icon} width={100} height={100} />
+        <Image src={logo} className={styles.icon} width={100} height={100} />
         <h3>Гороскопер</h3>
         <form onSubmit={onSubmit}>
           <input
